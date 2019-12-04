9a1ee349edec15d250af06bb9adc3581124f6aaa
Powderella <31888797+Powderella@users.noreply.github.com>
2019-12-04 17:24:33 +0900

    a

--- cogs/qiitacog.py
+++ cogs/qiitacog.py
@@ -7,15 +7,20 @@ class QiitaCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot        
+        
         self.defaultChannel = self.bot.get_channel(DISCORD_DEFAULT_CHANNEL)
         self.index = 0
-
         self.printQiitaArticleLatest.start()
-
     @tasks.loop(minutes=QIITA_LOOP_TIME)
     async def printQiitaArticleLatest(self):
         await self.defaultChannel.send(str(self.index))
         self.index += 1
 
+
+    @printQiitaArticleLatest.before_loop
+    async def beforePrintQiitaArticleLatest(self):
+        await self.bot.wait_until_ready()
+        self.defaultChannel = self.bot.get_channel(DISCORD_DEFAULT_CHANNEL)
+        print(self.bot)
 def setup(bot):
     bot.add_cog(QiitaCog(bot))
