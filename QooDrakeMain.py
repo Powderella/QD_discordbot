22c29a70b393a4095fe1de150b003303d740296f
Powderella <31888797+Powderella@users.noreply.github.com>
2019-12-04 16:26:37 +0900

    a

--- QooDrakeMain.py
+++ QooDrakeMain.py
@@ -8,6 +8,8 @@ from settings import DISCORD_TOKEN
 
 cogs = os.listdir("./cogs/")
 
+if os.path.exists("./db/"):
+    os.makedirs("./db/")
 INITIAL_COGS = ["cogs." + cog.strip(".py") for cog in cogs if cog.endswith(".py")]
 
 class QooDrakeMain(commands.Bot):
