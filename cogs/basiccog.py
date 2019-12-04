300aa746f72d484daa25810fb6ba15000d9edf11
Powderella <31888797+Powderella@users.noreply.github.com>
2019-12-04 16:16:09 +0900

    a

--- cogs/basiccog.py
+++ cogs/basiccog.py
@@ -1,9 +1,8 @@
 from discord.ext import commands # Bot Commands Frameworkのインポート
 import discord
-import shelve
 
 # コグとして用いるクラスを定義。
-class TestCog(commands.Cog):
+class BasicCog(commands.Cog):
 
     # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
     def __init__(self, bot):
