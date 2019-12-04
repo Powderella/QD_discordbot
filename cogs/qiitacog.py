94395cb3635bc80b4e49985409ef2546df68b629
Powderella <31888797+Powderella@users.noreply.github.com>
2019-12-04 17:30:05 +0900

    a

--- cogs/qiitacog.py
+++ cogs/qiitacog.py
@@ -1,26 +1,20 @@
 from discord.ext import commands, tasks
-
 from settings import DISCORD_DEFAULT_CHANNEL, QIITA_LOOP_TIME, DB_DIR
 
 
 class QiitaCog(commands.Cog):
 
-    def __init__(self, bot):
-        self.bot = bot        
-        
-        self.defaultChannel = self.bot.get_channel(DISCORD_DEFAULT_CHANNEL)
+    def __init__(self):
         self.index = 0
-        self.printQiitaArticleLatest.start()
-    @tasks.loop(minutes=QIITA_LOOP_TIME)
-    async def printQiitaArticleLatest(self):
-        await self.defaultChannel.send(str(self.index))
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(self.index)
         self.index += 1
 
-
-    @printQiitaArticleLatest.before_loop
-    async def beforePrintQiitaArticleLatest(self):
-        await self.bot.wait_until_ready()
-        self.defaultChannel = self.bot.get_channel(DISCORD_DEFAULT_CHANNEL)
-        print(self.bot)
 def setup(bot):
     bot.add_cog(QiitaCog(bot))
