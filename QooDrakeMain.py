75db2abd7c2d3846ef0b3fd05ec2f6005bf3860b
ぱうでれら <31888797+Powderella@users.noreply.github.com>
2021-06-25 23:44:00 +0900

    Update QooDrakeMain.py

--- QooDrakeMain.py
+++ QooDrakeMain.py
@@ -51,7 +51,7 @@ class QooDrakeMain(commands.Bot):
         """
         self.owner_id = DISCORD_OWNER_ID
         cusActivity = discord.CustomActivity("BEYOND THE PIEN", emoji="🥺")
-        await self.bot.change_presence(activity=cusActivity)
+        await self.change_presence(activity=cusActivity)
 
 if __name__ == "__main__":
     bot = QooDrakeMain(command_prefix="/")
